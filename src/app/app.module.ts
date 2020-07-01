bdb59d27ace3bc1b2957a789d54f0a4705a282f8
Zach <zacharyjordan82@gmail.com>
2020-06-30 22:58:26 -0400

    adding basic controls for event creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { EventComponent } from './event/event.component';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from "./home/home.component";
+import { EventComponent } from "./event/event.component";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { environment } from "src/environments/environment";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    EventComponent
-  ],
+  declarations: [AppComponent, HomeComponent, EventComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { v4 as uuid } from "uuid";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  formGroup: FormGroup;
+  minutes: number[] = [];
+  hours: number[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private firestore: AngularFirestore) {
+    this.formGroup = new FormGroup({
+      name: new FormControl("", [Validators.required]),
+      date: new FormControl("", [Validators.required]),
+      startHour: new FormControl("", [Validators.required]),
+      startMinute: new FormControl("", [Validators.required]),
+      endHour: new FormControl("", [Validators.required]),
+      endMinute: new FormControl("", [Validators.required]),
+    });
+    this.minutes = [0, 15, 30, 45];
+    for (let index = 1; index <= 24; index++) {
+      this.hours.push(index);
+    }
   }
 
+  ngOnInit(): void {}
+  createEvent() {
+    let name = this.formGroup.get("name").value;
+    let date = this.formGroup.get("date").value;
+    let startDate = new Date(date);
+    startDate.setHours(this.formGroup.get("startHour").value);
+    startDate.setMinutes(this.formGroup.get("startMinute").value);
+    let endDate = new Date(date);
+    endDate.setHours(this.formGroup.get("endHour").value);
+    endDate.setMinutes(this.formGroup.get("endMinute").value);
+    console.log(name);
+    this.firestore.collection("events").doc(uuid()).set({
+      name: name,
+      startDate: startDate,
+      endDate: endDate,
+    });
+  }
 }
